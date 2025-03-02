cdae012c0a21d92a96cf90427955866c9abfa884
Eilish McEwan <eilishmcewan@gmail.com>
2025-03-02 14:26:22 +1300

    spares and single strike

--- score.js
+++ score.js
@@ -1,5 +1,11 @@
 export { scoreFrame }
 
 function scoreFrame(Frame) {
-  return Frame[0] + Frame[1]
+  if (Frame[0] === 10) {
+    return 'Strike!'
+  } else if (Frame[0] + Frame[1] === 10) {
+    return 'Spare!'
+  } else {
+    return Frame[0] + Frame[1]
+  }
 }
--- score.test.js
+++ score.test.js
@@ -10,13 +10,27 @@ test('scores a gutterball frame', () => {
   const frame = [0, 0]
   const expected = 0
   const actual = score.scoreFrame(frame)
-
   expect(actual).toBe(expected)
 })
 
 test('scores a normal frame', () => {
   const frame = [2, 3]
   const expected = 5
+  const actual = score.scoreFrame(frame)
+  expect(actual).toBe(expected)
+})
+
+test('scores a spare', () => {
+  const frame = [3, 7]
+  const expected = 'Spare!'
+  const actual = score.scoreFrame(frame)
+  expect(actual).toBe(expected)
+})
+
+test('scores a single strike', () => {
+  let X = 10
+  const frame = [X, 0]
+  const expected = 'Strike!'
   const actual = score.scoreFrame(frame)
   expect(actual).toBe(expected)
 })
